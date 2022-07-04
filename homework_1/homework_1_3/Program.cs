e0f030b0390b1e0f1dccd4d76adb56fcb8e8923a
Ruslan Kharitonov <=>
2022-07-04 20:13:19 +0300

    merge all homeworks in one file, delete old projects, rename path

--- homework_1/homework_1_3/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.Clear();
-Console.Write("Input number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if (number % 2 == 0) Console.Write(number + " -> " + "да");
-else Console.Write(number + " -> " + "нет");
