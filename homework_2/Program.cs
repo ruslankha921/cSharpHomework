f4a2eb079acca0573561fc9b97cd6f9fb31a86b4
ruslan_kharitonov <ruslankha@mail.ru>
2022-07-07 19:53:43 +0300

    add homework_1 and homework_3 path to track

--- homework_2/Program.cs
+++ homework_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿// //homework_2_1
+// Console.Clear();
 // void secondNumber(){
 // Console.Write("Введите число: ");
 // int number = Convert.ToInt32(Console.ReadLine());
@@ -9,31 +10,45 @@
 
 
 //homework_2_2
-void bitDepth()
-{
-    Console.Write("Введите число : ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    int i = 0;
-    while (num > 0)
-    {
-        i++;
-        num /= 10;
-    }
-    Console.WriteLine("Количество цифр введенного числа : {0}", i);
-}
+Console.Clear();
+
+
+
+
+
 
 void thirdNumber()
 {
     Console.Write("Введите число: ");
     int number = Convert.ToInt32(Console.ReadLine());
+    void hundredth()
+    {
+    int i = 0;
+        while (number > 0)
+        {
+            i++;
+            number /= 10;
+        }
+    }
+  
+    // double mathPow = Math.Pow (10, num - 2);
 
     if (number < 100) Console.Write(number + " -> третьей цифры нет");
 
-    else if (number > 100)
+    else if (number > 99 && number < 1000)
+    {
+        Console.Write(number + " -> " + ((number - ((number / 100) * 100)) % 10));
+    }
+
+    else if (number > 999 && number < 10000)
     {
         Console.Write(number + " -> " + ((number - ((number / 100) * 100)) / 10));
     }
-
+    
+    else
+    {
+        Console.Write(number + " -> " + ((number - ((number / 1000) * 1000)) / 100));
+    }
 }
 
 thirdNumber();
