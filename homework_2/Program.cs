6ecb6f8c8acf26fe54d69bf68804a4a1c83e91eb
Ruslan Kharitonov <=>
2022-07-07 04:58:06 +0300

    add homework 2.2 is done

--- homework_2/Program.cs
+++ homework_2/Program.cs
@@ -1,7 +1,28 @@
-﻿void secondNumber(){
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.Write(number + " -> " + ((number - ((number / 100) * 100)) / 10));
+﻿// //homework_2_1
+// void secondNumber(){
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write(number + " -> " + ((number - ((number / 100) * 100)) / 10));
+// }
+
+// secondNumber();
+
+
+//homework_2_2
+void thirdNumber()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    if (number < 100) Console.Write(number + " -> третьей цифры нет");
+    
+    else if (number > 100)
+    {
+        Console.Write(number + " -> " + ((number - ((number / 100) * 100)) / 10));
+    }
+
 }
 
-secondNumber();
+thirdNumber();
+
+
