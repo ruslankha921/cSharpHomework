a2a870e81fa701c70b17ef5e4706e3b9af142c50
Ruslan Kharitonov <=>
2022-07-07 07:09:14 +0300

    add new code in homework 2.2

--- homework_2/Program.cs
+++ homework_2/Program.cs
@@ -9,13 +9,26 @@
 
 
 //homework_2_2
+void bitDepth()
+{
+    Console.Write("Введите число : ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int i = 0;
+    while (num > 0)
+    {
+        i++;
+        num /= 10;
+    }
+    Console.WriteLine("Количество цифр введенного числа : {0}", i);
+}
+
 void thirdNumber()
 {
     Console.Write("Введите число: ");
     int number = Convert.ToInt32(Console.ReadLine());
-    
+
     if (number < 100) Console.Write(number + " -> третьей цифры нет");
-    
+
     else if (number > 100)
     {
         Console.Write(number + " -> " + ((number - ((number / 100) * 100)) / 10));
